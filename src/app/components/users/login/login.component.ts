088566c48cfbf103b3b7165ccda62b7e1211e3c7
Gabriel <gaby13453@gmail.com>
2024-09-17 01:09:23 -0500

    feat: :sparkles: Login error added

--- src/app/components/users/login/login.component.ts
+++ src/app/components/users/login/login.component.ts
@@ -10,33 +10,32 @@ import { DarkModeToggleComponent } from '../../main/pages/header/dark-mode-toggl
   standalone: true,
   imports: [CommonModule, FormsModule, DarkModeToggleComponent, RouterLink],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
 })
 export class LoginComponent {
   loginCredentials = {
     email: '',
     password: '',
   };
+  errorMessage: string = '';
+
   constructor(
     private loginService: LoginService,
     private router: Router,
   ) { }
   
   onLogin() {
+    this.errorMessage = ''; // Limpiar mensaje de error anterior
     this.loginService.login(this.loginCredentials).subscribe({
       next: (response) => {
         console.log('Login exitoso', response);
-        // manejar el éxito del login, como guardar el token
         localStorage.setItem('authToken', response.token);
-        // Redirigir a la página de inicio
         this.router.navigate(['']);
       },
       error: (error) => {
         console.error('Error en el login', error);
-        // manejar los errores de login
+        this.errorMessage = 'Usuario o contraseña incorrectos';
       }
     });
   }
-  
 }
 
